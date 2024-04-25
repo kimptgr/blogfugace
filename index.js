16955ae0090969fb4b35f6b0b348c9365997c6c7
kimptgr <163475107+kimptgr@users.noreply.github.com>
2024-04-25 11:46:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+
+const app = express() ;
+
+app.use(express.urlencoded({extended:true})); 
+
+app.use(express.static("public")) ; 
+
+app.get("/", (req,res) => {
+    billets = [] ;
+    res.render("index.ejs")
+})
+
+let billets = [];
+let horodatage = [] ;
+
+app.post("/submit", (req, res) => {
+    let texte = req.body["billet"] ;
+    billets.push(texte) ;
+
+    let horaire = new Date() ;
+    function donneHorodatage() {
+        let joliMinutes = horaire.getMinutes()
+        if (horaire.getMinutes()< 10) {
+            joliMinutes = `0${joliMinutes}`
+        }
+        else {}        
+    return `le ${horaire.toLocaleDateString()} à ${horaire.getHours()}h${joliMinutes}`
+    }
+    horodatage.push(donneHorodatage())
+
+    res.render("index.ejs", {billets, horodatage})
+
+})
+
+const port = 3000 ;
+app.listen(port, () => {console.log(`Serveur running on port ${port}`)})
